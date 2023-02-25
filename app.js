2690915ac950cadfeedfe0b4a22f48458ba3645f
Munasar abuukar <110159068+munasarabokar@users.noreply.github.com>
2023-02-25 12:55:27 -0800

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
   secret:'munasar',
-  cookie: {maxAge : 60000},
+  cookie: {maxAge : 160000},
   saveUninitialized: true,
   resave: true
 }));
